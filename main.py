35c21757ed687922c6d77b2d5d3b10f20ec30dfe
ArtemZakhar <artemzakharchuk@gmail.com>
2026-01-08 08:08:04 +0200

    fix

--- main.py
+++ main.py
@@ -14,37 +14,37 @@ def main() -> None:
     with open("players.json", "r") as file:
         players_data = json.load(file)
 
-        for name, player_data in players_data.items():
-            guild = player_data.get("guild")
-            guild_data = None
+    for name, player_data in players_data.items():
+        guild = player_data.get("guild")
+        guild_data = None
 
-            if guild:
-                guild_data, _ = guilds.get_or_create(
-                    name=guild.get("name"),
-                    defaults={"description": guild.get("description", None)}
-                )
-
-            race = player_data.get("race")
-
-            race_data, _ = races.get_or_create(
-                name=race.get("name"),
-                defaults={"description": race.get("description", "")}
+        if guild:
+            guild_data, _ = guilds.get_or_create(
+                name=guild.get("name"),
+                defaults={"description": guild.get("description", None)}
             )
 
-            for skill in race.get("skills", []):
-                skills.get_or_create(
-                    name=skill.get("name"),
-                    bonus=skill.get("bonus"),
-                    race=race_data
-                )
+        race = player_data.get("race")
 
-            players_qs.create(
-                nickname=name,
-                email=player_data.get("email"),
-                bio=player_data.get("bio"),
-                race=race_data,
-                guild=guild_data,
+        race_data, _ = races.get_or_create(
+            name=race.get("name"),
+            defaults={"description": race.get("description", "")}
+        )
+
+        for skill in race.get("skills", []):
+            skills.get_or_create(
+                name=skill.get("name"),
+                bonus=skill.get("bonus"),
+                race=race_data
             )
+
+        players_qs.create(
+            nickname=name,
+            email=player_data.get("email"),
+            bio=player_data.get("bio"),
+            race=race_data,
+            guild=guild_data,
+        )
 
 
 if __name__ == "__main__":
